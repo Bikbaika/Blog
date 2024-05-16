689084a3e1baadce350b9f26c2a00406a9c20bb5
Bikbaika <kaleria4444@gmail.com>
2024-05-16 16:19:22 +0300

    Creating views for post datails and post list

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status" ]
+    search_fields = ['title']
+    list_filter=['status','author']
+    raw_id_fields=['author']
+    prepopulated_fields = {'slug':('title',)}
+
