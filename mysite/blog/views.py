67025f84e5280a27dc4e1ab34d55e84e4dbeff29
Bikbaika <kaleria4444@gmail.com>
2024-05-22 00:42:23 +0300

    Pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,24 @@
 from django.http import Http404
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
+from django.views.generic import ListView
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list,3)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
